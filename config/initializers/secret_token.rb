604882e6029ad820f218e2d410293f31f669462b
Lawson Kurtz <lawson.kurtz@viget.com>
2013-02-21 09:18:11 -0500

    Base Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airline::Application.config.secret_token = 'b1d1ed368298d38d0e5f19200ff0f1ec9fc1ee1869dc4333d97c1fed2b1814077f409b45f1cb6e9b8c290d47833fdc05ab5bab4150a6a48df0d61a9ca98798b2'
