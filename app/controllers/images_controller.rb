fe479bf01071212ad13f325208181abdbdbb62ba
Lawson Kurtz <lawson.kurtz@viget.com>
2013-03-05 13:51:00 -0500

    Edit per PR#6 comments
    - Creates scoped building on user for images
    - Eliminates unneeded images_helper.rb
    - Uses CSS selector to find elements in specs

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,20 +2,18 @@ class ImagesController < ApplicationController
   before_filter :ensure_authenticated
 
   def create
-    image = Image.new(params[:image])
-    image.user = current_user
-    image.company = current_user.company
+    image = current_user.build_image(params[:image])
 
-    if image.save
-      message = { :notice => "Image saved. Add another?" }
+    message = if image.save
+      { :notice => "Image saved. Add another?" }
     else
-      message = { :alert => "There were errors with your submission" }
+      { :alert => "There were errors with your submission" }
     end
 
     redirect_to root_path, message
   end
 
   def index
-    @images = current_user.image_source.images.newest_first.page(params[:page])
+    @images = current_user.image_source.images.newest.page(params[:page])
   end
 end
--- app/helpers/images_helper.rb
+++
@@ -1,5 +0,0 @@
-module ImagesHelper
-  def no_images_message
-    "(Crickets) There aren't any images here yet."
-  end
-end
