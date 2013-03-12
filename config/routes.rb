c60c40c961c71edbfe98cd3d9c1bb877b9eb1d70
Lawson Kurtz <lawson.kurtz@viget.com>
2013-03-12 16:48:33 -0400

    Remove logic from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,20 +12,8 @@ Airline::Application.routes.draw do
   resources :images, :only => [:create]
   resource :company, :only => [:show]
 
-  scope :constraints => lambda{|req| req.session[:user_id].present? } do
-    match "/:company_slug" => "company_image_streams#index", :as => :company_images
-    match "/:company_slug/:user_slug" => "user_image_streams#index", :as => :company_user_images
-    root :to => redirect { |p,req| company_feed_path(req) }
-  end
+  match "/:company_slug" => "company_image_streams#index", :as => :company_images
+  match "/:company_slug/:user_slug" => "user_image_streams#index", :as => :company_user_images
 
-  match "/:company_slug(/:user_slug)" => redirect("/")
   root :to => "homepage#show"
-
-  def company_feed_path(request)
-    "/#{request_user(request).company_slug}"
-  end
-
-  def request_user(request)
-    User.find(request.session[:user_id])
-  end
 end
